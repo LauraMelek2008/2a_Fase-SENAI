2c5c1c396bfeeb0c7d6520f5519e72d0ba85fec9
LauraMelek2008 <laura_c_nunes@estudante.sesisenai.org.br>
2024-08-23 17:28:21 -0300

    Criação do Primeiro projeto com React Bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>MINECRAFT</h1>
+      <h2>BLOCOS</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate architecto sint ex accusantium adipisci iure, eum, doloribus et assumenda commodi placeat necessitatibus quod rerum eos officia nihil dolor quos voluptatem.</p>
+    </>
+  )
+}
+
+export default App
