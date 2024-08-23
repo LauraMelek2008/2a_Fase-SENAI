599cf2c5603f9131720d431d7c1477350aee3b4c
LAURA CRISTINA MELEK NUNES <laura_c_nunes@estudante.sesisenai.org.br>
2024-08-23 17:18:36 -0300

    Criação do Primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+<h1>Minecraft</h1>
+
+<h2>Minerios</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor mollitia animi officia enim amet omnis dicta adipisci recusandae quos saepe voluptatem rem obcaecati pariatur, laboriosam doloremque quod a molestias.</p>
+<h2>Madeiras</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore sed corporis et recusandae eveniet dolorum alias eaque, voluptate itaque eum. Cumque porro nulla exercitationem nisi, fuga iure mollitia esse illum!</p>
+
+
+    </>
+  )
+}
+
+export default App
