9ebd0f5fac8735dafcf0b28ccf4cd6f102539f02
LauraMelek2008 <laura_c_nunes@estudante.sesisenai.org.br>
2024-08-29 17:24:44 -0300

    add criação de componentes

---
+++ componeteVDD/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Cabecalho from './components/Cabecalho'
+import Links from './components/Links'
+
+function App() {
+  
+
+  return (
+    <>
+      <Cabecalho /> 
+      <Links />
+
+
+    </>
+  )
+}
+
+export default App
---
+++ componeteVDD/src/components/Links.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Links() {
+  return (
+    <div>
+      <p>Link1| Link2| Link3| Link4| Link5| Link6|</p>
+    </div>
+  )
+}
+
+export default Links
